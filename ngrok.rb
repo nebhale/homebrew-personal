23ac926c636977cf81e4ddd018504eca0fa8be62
Ben Hale <bhale@gopivotal.com>
2013-12-06 12:04:19 +0000

    Update to ngrok 1.6
    This change updates ngrok 1.6 to build more smoothly.

--- ngrok.rb
+++ ngrok.rb
@@ -2,17 +2,15 @@ require 'formula'
 
 class Ngrok < Formula
   homepage 'https://ngrok.com'
-  url 'https://github.com/inconshreveable/ngrok/archive/1.6.tar.gz'
-  sha1 '03d076bfe078ebe52c0c81dfa1e49b497e7295fa'
+  url 'https://github.com/inconshreveable/ngrok.git', :tag => '1.6'
+  version '1.6'
+
+  head 'https://github.com/inconshreveable/ngrok.git', :branch => 'master'
 
   depends_on 'bazaar' => :build
   depends_on 'go' => :build
+  depends_on :python => :build
   depends_on MercurialDependency => :build
-
-  fails_with :clang do
-    build 425
-    cause "clang: error: no such file or directory: 'libgcc.a'"
-  end
 
   def install
     system 'make', 'release-client'
