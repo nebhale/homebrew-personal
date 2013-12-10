92bbc753ad26c6c4fe90698d5849cd7029720b51
Ben Hale <bhale@gopivotal.com>
2013-12-10 06:19:44 +0000

    ngrok 1.6
    This change updates the ngrok formula by removing the dependency on
    python.  This dependency was only required because of bad
    configuration on one of my computers.  In addition, the download URL
    has been reverted back to the GitHub tarball rather than a Git
    checkout.

--- ngrok.rb
+++ ngrok.rb
@@ -2,14 +2,13 @@ require 'formula'
 
 class Ngrok < Formula
   homepage 'https://ngrok.com'
-  url 'https://github.com/inconshreveable/ngrok.git', :tag => '1.6'
-  version '1.6'
+  url 'https://github.com/inconshreveable/ngrok/archive/1.6.tar.gz'
+  sha1 '03d076bfe078ebe52c0c81dfa1e49b497e7295fa'
 
   head 'https://github.com/inconshreveable/ngrok.git', :branch => 'master'
 
   depends_on 'bazaar' => :build
   depends_on 'go' => :build
-  depends_on :python => :build
   depends_on MercurialDependency => :build
 
   def install
