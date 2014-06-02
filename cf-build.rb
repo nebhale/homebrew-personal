4a9b84774decbe0157b5eb3d5eb668c9b5e70c34
Ben Hale <bhale@gopivotal.com>
2014-06-02 13:01:37 +0100

    cf-build v1.0.0
    This change updates the cf-build formula for the v1.0.0 release

--- cf-build.rb
+++ cf-build.rb
@@ -4,7 +4,7 @@ class CfBuild < Formula
   head "https://github.com/nebhale/cf-build.git"
   homepage "https://github.com/nebhale/cf-build"
   url "https://github.com/nebhale/cf-build/archive/v1.0.0.tar.gz"
-  sha1 "a44e19bd6eb610d51d3b4d87961b2ea3a7126f3e"
+  sha1 "51f52e81de95e385eb88217915531d8c7dbe7680"
 
   depends_on "cfenv" => :run
   depends_on "cloudfoundry-cli" => :run
