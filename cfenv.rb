0775a696ae1f902fd24bdc072a1f24a8e42e31c9
Ben Hale <bhale@gopivotal.com>
2014-06-02 12:39:13 +0100

    cfenv v1.0.0
    This change updates the cfenv formula for the v1.0.0 release

--- cfenv.rb
+++ cfenv.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Cfenv < Formula
   homepage 'https://github.com/nebhale/cfenv'
   url 'https://github.com/nebhale/cfenv/archive/v1.0.0.tar.gz'
-  sha1 '825ceec55805b8bb80a6d6003fd3cef824d7ff14'
+  sha1 '5e46ebed93540470d207ff404b70bff6af4a87d9'
 
   head 'https://github.com/nebhale/cfenv.git'
 
