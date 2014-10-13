26db3d53ef09d1a0857b54f908d0d4fefa8b5086
Ben Hale <bhale@pivotal.io>
2014-10-13 10:15:31 +0100

    Cleanup
    This change cleans up the formula a bit.

--- cf-build.rb
+++ cf-build.rb
@@ -1,20 +1,20 @@
-require "formula"
+require 'formula'
 
 class CfBuild < Formula
-  head "https://github.com/nebhale/cf-build.git"
-  homepage "https://github.com/nebhale/cf-build"
-  url "https://github.com/nebhale/cf-build/archive/v1.0.0.tar.gz"
-  sha1 "51f52e81de95e385eb88217915531d8c7dbe7680"
+  head 'https://github.com/nebhale/cf-build.git'
+  homepage 'https://github.com/nebhale/cf-build'
+  url 'https://github.com/nebhale/cf-build/archive/v1.0.0.tar.gz'
+  sha1 '51f52e81de95e385eb88217915531d8c7dbe7680'
 
-  depends_on "cfenv" => :run
-  depends_on "cloudfoundry-cli" => :run
+  depends_on 'cfenv' => :run
+  depends_on 'cloudfoundry-cli' => :run
 
   def install
-    ENV["PREFIX"] = prefix
-    system "./install.sh"
+    ENV['PREFIX'] = prefix
+    system './install.sh'
   end
 
   test do
-    system "#{bin}/cf-build", "--version"
+    system "#{bin}/cf-build", '--version'
   end
 end
